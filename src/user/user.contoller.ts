754fb272fbee2378d69bd64ebfd28eefa7acefd3
Yuval Assa <assa.yuval107@gmail.com>
2018-12-27 16:10:59 +0200

    refactor: Remove unused imports and functions

--- src/user/user.contoller.ts
+++ src/user/user.contoller.ts
@@ -1,33 +1,15 @@
 import { Request, Response } from 'express';
 import { UserManager } from './user.manager';
-
 import { UserNotFoundError } from '../utils/errors/userErrors';
-import { UpdateWriteOpResult } from 'mongodb';
 
-type UpdateResponse = UpdateWriteOpResult['result'];
 export class UserController {
     static async create(req: Request, res: Response) {
         res.json(await UserManager.create(req.body));
     }
 
-    static async createMany(req: Request, res: Response) {
-        res.json(await UserManager.createMany(req.body));
-    }
-
     static async updateById(req: Request, res: Response) {
         const updated = await UserManager.updateById(req.params.id, req.body.user);
         if (!updated) {
-            throw new UserNotFoundError();
-        }
-
-        res.json(updated);
-    }
-
-    static async updateMany(req: Request, res: Response) {
-
-        const updated: UpdateResponse = await UserManager.updateMany(req.query, req.body);
-
-        if (updated.n === 0) {
             throw new UserNotFoundError();
         }
 
