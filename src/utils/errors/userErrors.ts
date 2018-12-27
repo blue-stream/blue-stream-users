5385b40cf7fc57ec44f00277937dba56157f969c
Yuval Assa <assa.yuval107@gmail.com>
2018-12-27 16:10:17 +0200

    feat: Add name and mail invalid errors

--- src/utils/errors/userErrors.ts
+++ src/utils/errors/userErrors.ts
@@ -12,6 +12,18 @@ export class IdInvalidError extends UserError {
     }
 }
 
+export class NameInvalidError extends UserError {
+    constructor(message?: string) {
+        super(message || 'Name is invalid', 400);
+    }
+}
+
+export class MailInvalidError extends UserError {
+    constructor(message?: string) {
+        super(message || 'Mail is invalid', 400);
+    }
+}
+
 export class UserNotFoundError extends UserError {
     constructor(message?: string) {
         super(message || 'User not found', 404);
