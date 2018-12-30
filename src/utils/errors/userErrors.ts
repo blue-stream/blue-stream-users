1bb1005ef2be67a45ad619a83b38e4138853e05d
Yuval Assa <assa.yuval107@gmail.com>
2018-12-30 16:18:37 +0200

    style: remove unused error

--- src/utils/errors/userErrors.ts
+++ src/utils/errors/userErrors.ts
@@ -1,11 +1,5 @@
 import { UserError } from './applicationError';
 
-export class PropertyInvalidError extends UserError {
-    constructor(message?: string) {
-        super(message || 'Property is invalid', 400);
-    }
-}
-
 export class IdInvalidError extends UserError {
     constructor(message?: string) {
         super(message || 'Id is invalid', 400);
