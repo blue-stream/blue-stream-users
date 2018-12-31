8907069f0a27f17c9525b2814635f2b041fa5d4e
Yuval Assa <assa.yuval107@gmail.com>
2018-12-31 16:39:36 +0200

    feat: Add getmany function

--- src/user/user.contoller.ts
+++ src/user/user.contoller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { UserManager } from './user.manager';
 import { UserNotFoundError } from '../utils/errors/userErrors';
+import { IUser } from './user.interface';
 
 export class UserController {
     static async create(req: Request, res: Response) {
@@ -44,7 +45,27 @@ export class UserController {
     }
 
     static async getMany(req: Request, res: Response) {
-        res.json(await UserManager.getMany(req.query));
+        let sortOrder: string;
+        let sortBy: string;
+        let startIndex: number;
+        let endIndex: number;
+        const userFilter: Partial<IUser> = {
+            firstName: req.query.firstname,
+            lastName: req.query.lastname,
+            mail: req.query.mail,
+        };
+
+        req.query.startIndex ? startIndex = req.query.startIndex : startIndex = 0;
+        req.query.endIndex ? endIndex = req.query.endIndex : endIndex = 20;
+        req.query.sortOrder ? sortOrder = req.query.sortOrder : sortOrder = '-';
+        req.query.sortBy ? sortBy = req.query.sortBy : sortBy = 'createdAt';
+
+        Object.keys(userFilter).forEach((key: string) => {
+            return userFilter[key as keyof IUser] ===
+                undefined && delete userFilter[key as keyof IUser];
+        });
+
+        res.json(await UserManager.getMany(req.query, startIndex, endIndex, sortOrder, sortBy));
     }
 
     static async getAmount(req: Request, res: Response) {
