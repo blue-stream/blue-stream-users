9bec2ee7e39e4b48aaedae71a0a855db4830814f
Ron Borysovski <roni537@gmail.com>
2019-02-14 11:24:22 +0200

    style: Change string concating to string interpolation

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -52,7 +52,7 @@ userSchema.virtual('id').set(function (this: IUser & mongoose.Document, id: stri
 });
 
 userSchema.virtual('name').get(function (this: IUser & mongoose.Document) {
-    return this.firstName + ' ' + this.lastName;
+    return `${this.firstName} ${this.lastName}`;
 });
 
 export const UserModel = mongoose.model<IUser & mongoose.Document>('User', userSchema);
